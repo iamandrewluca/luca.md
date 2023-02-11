063e2eace0cd109550565f0214cd25ca9f6b2183
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2023-02-11 18:05:22 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ export default defineApp({
 		link: [
 			{ href: "/favicon.png", type: "image/png", rel: "icon" },
 			{ href: "https://fosstodon.org/@iamandrewluca", rel: "me" },
+			{ href: "https://mstdn.md/@iamandrewluca", rel: "me" },
 		],
 		script: [{ src: "https://cdn.splitbee.io/sb.js", defer: true }],
 	},
