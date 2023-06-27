7bbd2bfe82201cc6aac7695457c637991621c260
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2023-06-28 01:01:38 +0300

    feat: add astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://iamandrewluca.com/",
-	integrations: [tailwind(), react()],
+	integrations: [tailwind(), react(), image()],
 });
