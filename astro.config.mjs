810b1c5212713ec09cfcb79f9991f13d61a81347
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2023-09-17 16:55:40 +0300

    chore: upgrade deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://iamandrewluca.com/",
-	integrations: [tailwind(), react(), image()],
+	integrations: [react(), tailwind()],
 });
