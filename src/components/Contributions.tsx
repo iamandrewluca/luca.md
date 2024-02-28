ea2b90e529a1f1ce463ad759ec83a1df66d9b8a4
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2024-02-29 00:41:41 +0200

    refactor: try a different approach

--- src/components/Contributions.tsx
+++ src/components/Contributions.tsx
@@ -1,13 +1,12 @@
-import { type FunctionComponent, cloneElement } from "react";
-import RAC, { type Props } from "react-activity-calendar";
 import { providers } from "../utils/providers";
-import data from "../data.json";
 import { intersperse } from "../utils";
+import type { ReactNode } from "react";
 
-// @ts-expect-error library has a strange CJS export that changes on dev/prod
-let Calendar: FunctionComponent<Props> = RAC.default ?? RAC;
+type ContributionsProps = {
+	children: ReactNode;
+};
 
-export function Contributions() {
+export function Contributions({ children: calendar }: ContributionsProps) {
 	let providersElements = providers.map((provider) => (
 		<li key={provider.id}>
 			{provider.access === "public" && (
@@ -38,22 +37,7 @@ export function Contributions() {
 	return (
 		<div className="overflow-x-auto rounded-2xl bg-white shadow-2xl">
 			<div className="min-h-[259px] min-w-[924px] p-10">
-				<Calendar
-					data={data}
-					theme={{
-						light: ["#ebedf0", "#9be9a8", "#40c463", "#30a14e", "#216e39"],
-					}}
-					labels={{
-						totalCount: `{{count}} contributions in the last year 😱`,
-					}}
-					colorScheme="light"
-					renderBlock={(element, activity) => {
-						// Do not inline `title`
-						// Warning: A title element received an array with more than 1 element as children.
-						let title = `${activity.count} contributions on ${activity.date}`;
-						return cloneElement(element, {}, <title>{title}</title>);
-					}}
-				/>
+				{calendar}
 
 				<div className="text-sm">
 					<span>Sources:</span>
