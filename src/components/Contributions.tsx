9e38e00d0ab625a96a1e385fe69bb48d9c136713
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2024-02-28 20:29:26 +0200

    refactor: add intersperse util for list items

--- src/components/Contributions.tsx
+++ src/components/Contributions.tsx
@@ -2,11 +2,35 @@ import { type FunctionComponent, cloneElement } from "react";
 import RAC, { type Props } from "react-activity-calendar";
 import { providers } from "../utils/providers";
 import data from "../data.json";
+import { intersperse } from "../utils";
 
 // @ts-expect-error library has a strange CJS export that changes on dev/prod
 let Calendar: FunctionComponent<Props> = RAC.default ?? RAC;
 
 export function Contributions() {
+	const providerElements = providers.map((provider) => (
+		<li key={provider.id}>
+			{provider.access === "public" && (
+				<a
+					href={`${provider.origin}/${provider.username}`}
+					target="_blank"
+					rel="noopener"
+					className="text-blue-800 underline"
+				>
+					{provider.name}
+				</a>
+			)}
+
+			{provider.access === "file" && (
+				<span className="cursor-not-allowed text-blue-800">
+					{provider.name}
+				</span>
+			)}
+		</li>
+	));
+
+	const dividerElement = <li className="-ml-1">,</li>;
+
 	return (
 		<div className="overflow-x-auto rounded-2xl bg-white shadow-2xl">
 			<div className="min-h-[259px] min-w-[924px] p-10">
@@ -30,26 +54,7 @@ export function Contributions() {
 				<div className="text-sm">
 					<span>Sources:</span>
 					<ul className="ml-1 inline-flex gap-1">
-						{providers.map((provider, index) => (
-							<li key={provider.id}>
-								{provider.access === "public" && (
-									<a
-										href={`${provider.origin}/${provider.username}`}
-										target="_blank"
-										rel="noopener"
-										className="text-blue-800 underline"
-									>
-										{provider.name}
-									</a>
-								)}
-								{provider.access === "file" && (
-									<span className="cursor-not-allowed text-blue-800">
-										{provider.name}
-									</span>
-								)}
-								{index < providers.length - 1 && <span>,</span>}
-							</li>
-						))}
+						{intersperse(providerElements, dividerElement)}
 					</ul>
 				</div>
 			</div>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,6 @@
 export let avatarUrl = `https://gravatar.com/avatar/3d8d36a4c7e54969b9659a9f46d2f392?size=256`;
+
+export let intersperse = <T>(a: T[], s: T): T[] => {
+	// @ts-ignore
+	return [...Array(2 * a.length - 1)].map((_, i) => (i % 2 ? s : a[i / 2]));
+};
