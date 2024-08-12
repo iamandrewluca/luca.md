96b0fa682ed2760b33a9a4dacdad5f5417f96089
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2024-08-12 20:21:19 +0300

    fix: adjust local avatar

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,3 @@
-export let avatarUrl = `https://gravatar.com/avatar/3d8d36a4c7e54969b9659a9f46d2f392?size=256`;
-
 type TypeOrFunction<T> = T | ((index: number) => T);
 
 export let intersperse = <T>(a: T[], s: TypeOrFunction<T>): T[] => {
